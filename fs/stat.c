7dacf39226268e5ed68052661fbbae26cd27b592
Mohammed Ahmed <mohammedaelnaggar1@gmail.com>
2025-07-11 20:35:42 +0200

    Revert "KernelSU: Allow init exec ksud under nosuid"
    This reverts commit 940c517ab20ba0926abc27b78986962256604901.

--- fs/stat.c
+++ fs/stat.c
@@ -37,10 +37,6 @@ void generic_fillattr(struct inode *inode, struct kstat *stat)
 
 EXPORT_SYMBOL(generic_fillattr);
 
-#ifdef CONFIG_KSU
-extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);
-#endif
-
 /**
  * vfs_getattr_nosec - getattr without security checks
  * @path: file to get attributes from
@@ -98,9 +94,7 @@ int vfs_fstatat(int dfd, const char __user *filename, struct kstat *stat,
 	struct path path;
 	int error = -EINVAL;
 	unsigned int lookup_flags = 0;
-#ifdef CONFIG_KSU
-        ksu_handle_stat(&dfd, &filename, &flags); // call KSU hook first
-#endif
+	ksu_handle_stat(&dfd, &filename, &flag);
 
 	if ((flag & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |
 		      AT_EMPTY_PATH)) != 0)
